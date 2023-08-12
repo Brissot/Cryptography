348085196d28178418aef7bfb24c11cb85053d3d
Brissot <sidsu7@gmail.com>
2023-08-12 16:39:33 -0400

    More Representative Tests
    Before it was immediately modding out to 0, which made the test case a
    trivial test case

--- Caesar/test.sh
+++ Caesar/test.sh
@@ -2,7 +2,7 @@
 
 echo "Test I - TestCaesar, single steps" > timing.txt
 echo "time (./TestCaesar war_n_peace.txt 600 > /dev/null)" >> timing.txt
-{ time (./TestCaesar war_n_peace.txt 26 > /dev/null); } 2>> timing.txt
+{ time (./TestCaesar war_n_peace.txt 15 > /dev/null); } 2>> timing.txt
 printf "\n\n\n" >> timing.txt
 
 # maybe it takes some time for the os to free resources?
@@ -11,5 +11,5 @@ sleep 5
 
 echo "Test II - TestCaesarII, giant steps" >> timing.txt
 echo "time (./TestCaesar2 war_n_peace.txt 600 > /dev/null)" >> timing.txt
-{ time (./TestCaesar2 war_n_peace.txt 26 > /dev/null); } 2>> timing.txt
+{ time (./TestCaesar2 war_n_peace.txt 15 > /dev/null); } 2>> timing.txt
 printf "\n\n\n" >> timing.txt
