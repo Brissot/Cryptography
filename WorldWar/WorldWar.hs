bb5b5ce023246c5e1f747aa1a47b5d0e31026363
Brissot <sidsu7@gmail.com>
2023-09-05 14:40:51 -0400

    Template for Playfair and Enigma

---
+++ WorldWar/WorldWar.hs
@@ -0,0 +1,8 @@
+-- functions to encrypt the cryptography used in the World Wars.
+-- All unsafe, but nonetheless interesting
+
+encodePlayfair :: [Char] -> [[Int]] -> [Char]
+decodePlayfair :: [Char] -> [[Int]] -> [Char]
+
+encodeEnigma :: [Char] -> [Char] -> [Char]
+decodeEnigma :: [Char] -> [Char] -> [Char]
